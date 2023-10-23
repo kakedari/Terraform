9e2b58782093289e1da4f98989127671cbebcb5a
kakedari <87064619+kakedari@users.noreply.github.com>
2023-10-23 12:24:32 +0530

    First terraform practical

---
+++ Terraform_01/ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "my-ec2" {
+  ami           = "ami-079b5e5b3971bd10d"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Created_By_Terraform"
+  }
+
+}
