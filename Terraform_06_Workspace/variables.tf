7b2c067e66483d00e473333314a87196a0688e19
kakedari <87064619+kakedari@users.noreply.github.com>
2023-10-25 15:30:49 +0530

    Terraform workspace

---
+++ Terraform_06_Workspace/variables.tf
@@ -0,0 +1,48 @@
+variable "security_group_name" {
+  type = map(any)
+  default = {
+    "default" = "default-workspace-sg"
+    "dev"     = "dev-workspsace-sg"
+    "qa"      = "qa-workspace-qa"
+    "staging" = "staging-workspace-qa"
+    "prod"    = "prod-workspace-sg"
+  }
+}
+
+variable "instance_type" {
+  type = map(any)
+  default = {
+    "default" = "t2.nano"
+    "dev"     = "t2.micro"
+    "qa"      = "t2.small"
+    "staging" = "t2.large"
+    "prod"    = "t2.2xlarge"
+  }
+}
+
+variable "ami_id" {
+  type    = string
+  default = "ami-04893cdb768d0f9ee"
+}
+
+variable "volume_type" {
+  type    = string
+  default = "gp2"
+}
+
+variable "ec2_key_name" {
+  type    = string
+  default = "new-key"
+}
+
+variable "subnet_name" {
+  type    = string
+  default = "subnet-0c848c41f64a0e53f"
+}
+
+variable "tag_values" {
+  type = map(any)
+  default = {
+    "Name" = "workspace-demo"
+  }
+}
